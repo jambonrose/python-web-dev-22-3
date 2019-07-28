617f16540c888b897e9478976db64da6742cae87
Andrew Pinkham <code@andrewsforge.com>
2019-07-28 17:53:57 -0400

    1.03 Fail test: add URL path and view for status

--- src/testapp/urls.py
+++ src/testapp/urls.py
@@ -1,6 +1,9 @@
 """URL paths for Test App"""
 from django.urls import path
 
-from .views import pong
+from .views import Status, pong
 
-urlpatterns = [path("ping/", pong, name="ping")]
+urlpatterns = [
+    path("ping/", pong, name="ping"),
+    path("status/", Status.as_view(), name="site_status"),
+]
--- src/testapp/views.py
+++ src/testapp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.http import (
     require_http_methods,
 )
+from django.views.generic import TemplateView
 
 
 @require_http_methods(["GET", "HEAD", "OPTIONS"])
@@ -16,3 +17,9 @@ def pong(request):
             ["GET", "HEAD", "OPTIONS"]
         )
         return response
+
+
+class Status(TemplateView):
+    """Provide a basic status page for users"""
+
+    template_name = "testapp/status.html"
