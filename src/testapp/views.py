dbb021ca24728511d65a8d4f30db11c145effd1f
Andrew Pinkham <code@andrewsforge.com>
2019-07-28 16:55:06 -0400

    1.02 Fail test: add HTTP decorator to view

--- src/testapp/views.py
+++ src/testapp/views.py
@@ -1,7 +1,11 @@
 """Views for the Test app"""
 from django.http import HttpResponse
+from django.views.decorators.http import (
+    require_http_methods,
+)
 
 
+@require_http_methods(["GET", "HEAD", "OPTIONS"])
 def pong(request):
     """Respond to ping requests"""
     return HttpResponse("pong")
