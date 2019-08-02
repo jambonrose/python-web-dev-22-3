f974e29979144a4371244fd896ec3679a4f05abc
Andrew Pinkham <code@andrewsforge.com>
2019-08-02 10:54:57 -0400

    1.04 Pass: Tag.name unique in DB

--- src/testapp/models.py
+++ src/testapp/models.py
@@ -6,7 +6,7 @@ from django_extensions.db.fields import AutoSlugField
 class Tag(Model):
     """Labels to help categorize data"""
 
-    name = CharField(max_length=31)
+    name = CharField(max_length=31, unique=True)
     slug = AutoSlugField(
         help_text="A label for URL config.",
         max_length=31,
