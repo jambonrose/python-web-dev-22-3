4d131a7bf59a53b464f35c1c0c3395d6e71abad5
Andrew Pinkham <code@andrewsforge.com>
2019-08-14 11:38:13 -0400

    2.10 Write test for password reset view

--- src/user/tests/test_password_views.py
+++ src/user/tests/test_password_views.py
@@ -1,4 +1,10 @@
 """Test authentication views"""
+from re import search as re_search
+
+from django.contrib.auth.views import (
+    INTERNAL_RESET_URL_TOKEN,
+)
+from django.core import mail
 from django.urls import reverse
 from test_plus import TestCase
 
@@ -72,3 +78,71 @@ class PasswordViewTests(TestCase):
                 "Password Changed Successfully",
                 [str(m) for m in self.context["messages"]],
             )
+
+    def test_password_reset_get(self):
+        """May anonymous users access password reset?"""
+        response = self.get_check_200("auth:password_reset")
+        templates = [
+            "base.html",
+            "user/base.html",
+            "user/password_reset_form.html",
+        ]
+        for t_name in templates:
+            with self.subTest(template=t_name):
+                self.assertTemplateUsed(response, t_name)
+
+    def test_password_reset(self):
+        """Can an anonymous user issue a password reset?"""
+        email = self.user.email
+        post_response = self.post(
+            "auth:password_reset", data={"email": email}
+        )
+        self.assertRedirects(
+            post_response, reverse("auth:login")
+        )
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].to, [email])
+        self.assertEqual(
+            mail.outbox[0].subject,
+            "Password reset on testserver",
+        )
+        urlmatch = re_search(
+            r"https?://[^/]*(/.*reset/\S*)",
+            mail.outbox[0].body,
+        )
+        self.assertIsNotNone(
+            urlmatch, "No URL found in sent email"
+        )
+
+        url_path = urlmatch.groups()[0]
+        *_, uidb64, token = filter(
+            None, url_path.split("/")
+        )
+        self.assertEqual(
+            reverse(
+                "auth:password_reset_confirm",
+                kwargs={"uidb64": uidb64, "token": token},
+            ),
+            url_path,
+        )
+        reset_get_response = self.get(url_path)
+        # starting in Django 1.11,
+        # Django redirects to different token to avoid
+        # leaking secret to third-parties included in HTML
+        # https://docs.djangoproject.com/en/stable/releases/1.11/#django-contrib-auth
+        self.assertRedirects(
+            reset_get_response,
+            reverse(
+                "auth:password_reset_confirm",
+                kwargs={
+                    "uidb64": uidb64,
+                    "token": INTERNAL_RESET_URL_TOKEN,
+                },
+            ),
+        )
+        # code below explicitly checks for link
+        # but! asserRedirects checks this by default
+        # making this test redundant!
+        url_path = reset_get_response.url
+        reset_get_response = self.get_check_200(url_path)
