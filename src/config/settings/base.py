4636a54e123358a56541562c0f5c405d73586f23
Andrew Pinkham <code@andrewsforge.com>
2019-08-22 17:03:00 -0700

    5.01 Load staticfiles template tags by default

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -67,12 +67,15 @@ TEMPLATES = [
         "DIRS": [BASE_DIR("templates")],
         "APP_DIRS": True,
         "OPTIONS": {
+            "builtins": [
+                "django.contrib.staticfiles.templatetags.staticfiles"
+            ],
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ]
+            ],
         },
     }
 ]
